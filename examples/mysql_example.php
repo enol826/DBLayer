d8c91e29ba0f77e68e8696dd549ad27f2459e1d9
Enol <enol826@hotmail.com>
2018-02-19 14:35:39 +0100

    Update mysql_example.php

--- examples/mysql_example.php
+++ examples/mysql_example.php
@@ -4,14 +4,14 @@
 $object = new Mysql();
 try{
     // Connection
-      $object->connection("127.0.0.1", "root","","prueba");
+      $object->connection("127.0.0.1", "root","password","database");
     // Executing query and getting result set in $response
       $response = $object->query("SELECT * FROM users" );
     print "Number of rows returned: ".$response->num_rows().'<br/>';
     // Looping through each row
       for($i=0;$i<$response->num_rows();$i++) {
           $f = $response->fetch_array();
-          print "Name of the user: ". $f['name1'].'<br/>';
+          print "Name of the user: ". $f['name'].'<br/>';
       }
 }catch(DBException $ex) {
     print $ex->getMessage();
